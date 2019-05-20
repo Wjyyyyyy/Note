c4c0e35f122a8f9e958a28682d99377b0d4c0e02
Wjyyyyyy <48470942+Wjyyyyyy@users.noreply.github.com>
2019-05-20 20:42:57 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/administrator/mynotepad/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.mynotepad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, NoteEditor.class);
+        startActivity(intent);
+
+    }
+}
